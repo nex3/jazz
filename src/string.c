443f9920714b4bebfcf5b0739e7c4659145992e6
Nathan Weizenbaum <nex342@gmail.com>
2007-12-19 23:06:42 -0800

    Added a jz_str_to_chars function.

--- src/string.c
+++ src/string.c
@@ -1,4 +1,5 @@
 #include "string.h"
+#include <stdlib.h>
 
 jz_str jz_str_new(int length, UChar* value) {
   jz_str to_ret;
@@ -9,4 +10,11 @@ jz_str jz_str_new(int length, UChar* value) {
 
 jz_str jz_str_substr(jz_str this, int start, int length) {
   return jz_str_new(length, this.value + start);
+}
+
+char* jz_str_to_chars(jz_str this) {
+  char* num = calloc(this.length + 1, sizeof(char));
+  u_UCharsToChars(this.value, num, this.length);
+  num[this.length] = '\0';
+  return num;
 }
