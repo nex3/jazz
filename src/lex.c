64435b90b83ff05962c2d3da3c3b409ac663bb54
Nathan Weizenbaum <nex342@gmail.com>
2007-12-20 00:57:07 -0800

    Add lexer support for punctuation.

--- src/lex.c
+++ src/lex.c
@@ -8,15 +8,20 @@
 
 #include <unicode/uregex.h>
 
+// struct hash_result is defined in keywords.gperf
+typedef struct hash_result hash_result;
+
 static struct {
   jz_str code;
   jz_str code_prev;
   URegularExpression* identifier_re;
   URegularExpression* whitespace_re;
+  URegularExpression* punctuation_re;
   URegularExpression* decimal_literal_re;
 } state;
 
 static bool try_decimal_literal();
+static int try_punctuation();
 static bool try_re(URegularExpression* re);
 static jz_str get_match(URegularExpression* re, int number);
 
@@ -29,8 +34,10 @@ static void check_error(UErrorCode error) {
 int yylex() {
   try_re(state.whitespace_re);
 
+  int res;
   if (try_re(state.identifier_re)) return IDENTIFIER;
   if (try_decimal_literal()) return NUMBER;
+  if ((res = try_punctuation())) return res;
   return 0;
 }
 
@@ -40,6 +47,17 @@ bool try_decimal_literal() {
   yylval.num = (float)(atoi(num));
   free(num);
   return true;
+}
+
+int try_punctuation() {
+  if (!try_re(state.punctuation_re)) return false;
+  jz_str jz_match = get_match(state.punctuation_re, 0);
+  char* match = jz_str_to_chars(jz_match);
+  const hash_result* result = in_word_set(match, jz_match.length);
+  if (result) return result->token;
+  
+  printf("Lexer error: Unrecognized punctuation %s\n", match);
+  return false;
 }
 
 bool try_re(URegularExpression* re) {
@@ -83,6 +101,9 @@ void jz_lex_set_code(jz_str code) {
 void jz_lex_init() {
   state.identifier_re      = create_re("\\A" IDENTIFIER_START_RE IDENTIFIER_PART_RE "+");
   state.whitespace_re      = create_re("\\A[\\p{Zs}\\t\\x0B\\f]");
+  state.punctuation_re     = create_re("(?:[\\{\\}\\(\\)\\[\\]\\.;,\\*%!~\\?:\\^\\/]|"
+                                       ">=|<=|!==?|\\+=|-=|\\*=|%=|<<=|>>=|&=|\\|=|\\^=|\\/=|"
+                                       "<<?|>{1,3}|={1,3}|\\+\\+?|--?|&&?|\\|\\|?)");
   state.decimal_literal_re = create_re(DECIMAL_INTEGER_LITERAL_RE);
 }
 
