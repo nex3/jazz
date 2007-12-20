9d630d4137bed335594c4fb3bd434f640eb1eba0
Nathan Weizenbaum <nex342@gmail.com>
2007-12-20 02:15:56 -0800

    Fix a resource leak.

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ int main(int argc, char** argv) {
   UFILE* ustdin = u_finit(stdin, NULL, NULL);
   UChar str[2000];
   int32_t len = u_file_read(str, 2000, ustdin);
+  u_fclose(ustdin);
   jz_str input = jz_str_new(len, str);
 
   jz_parse_node* root = jz_parse_string(input);
