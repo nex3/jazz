fdc526bca0eea41d7d4f2c9be25fff826ddce01e
Nathan Weizenbaum <nex342@gmail.com>
2007-12-20 23:53:58 -0800

    Fix jz_str_to_chars - transform using the to-utf8 function.

--- src/string.c
+++ src/string.c
@@ -13,8 +13,9 @@ jz_str jz_str_substr(jz_str this, int start, int length) {
 }
 
 char* jz_str_to_chars(jz_str this) {
-  char* num = calloc(this.length + 1, sizeof(char));
-  u_UCharsToChars(this.value, num, this.length);
-  num[this.length] = '\0';
-  return num;
+  UErrorCode error = U_ZERO_ERROR;
+  char* to_ret = calloc(this.length + 1, sizeof(char));
+  u_strToUTF8(to_ret, this.length, NULL, this.value, this.length, &error);
+  to_ret[this.length] = '\0';
+  return to_ret;
 }
