a08528456aefa62ea3436a5609b12077e13f3f5d
Nathan Weizenbaum <nex342@gmail.com>
2007-12-22 17:45:58 -0800

    Removing an extraneous backslash.

---
+++ src/type.h
@@ -0,0 +1,24 @@
+#ifndef JZ_OBJECT_H
+#define JZ_OBJECT_H
+
+#include <stdbool.h>
+
+enum {
+  jz_num,
+  jz_bool
+} jz_type_type;
+
+typedef union {
+  double num;
+  bool b;
+} jz_value;
+
+typedef struct {
+  unsigned char type;
+  jz_value value;
+} jz_tvalue;
+
+jz_tvalue jz_wrap_num(double num);
+jz_tvalue jz_wrap_bool(bool b);
+
+#endif
