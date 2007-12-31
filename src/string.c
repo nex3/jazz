ca1bb4ec666dc5046de620aedf50b0081e6d9596
Nathan Weizenbaum <nex342@gmail.com>
2007-12-31 09:59:09 -0800

    String documentation and const correctness.

--- src/string.c
+++ src/string.c
@@ -3,22 +3,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-jz_str* jz_str_new(int length, UChar* value) {
+jz_str* jz_str_new(int length, const UChar* value) {
   jz_str* to_ret = malloc(sizeof(jz_str));
   to_ret->length = length;
   to_ret->value = value;
   return to_ret;
 }
 
-jz_str* jz_str_deep_new(int length, UChar* value) {
+jz_str* jz_str_deep_new(int length, const UChar* value) {
   jz_str* to_ret;
+  UChar* buffer;
 
   if (length == 0) return jz_str_null();
 
   to_ret = malloc(sizeof(jz_str));
   to_ret->length = length;
-  to_ret->value = calloc(sizeof(UChar), length);
-  memcpy(to_ret->value, value, length * sizeof(UChar));
+
+  value = calloc(sizeof(UChar), length);
+  memcpy(buffer, value, length * sizeof(UChar));
+  to_ret->value = buffer;
+
   return to_ret;
 }
 
@@ -26,15 +30,15 @@ jz_str* jz_str_null() {
   return jz_str_new(0, NULL);
 }
 
-jz_str* jz_str_dup(jz_str* this) {
+jz_str* jz_str_dup(const jz_str* this) {
   return jz_str_new(this->length, this->value);
 }
 
-jz_str* jz_str_deep_dup(jz_str* this) {
+jz_str* jz_str_deep_dup(const jz_str* this) {
   return jz_str_deep_new(this->length, this->value);
 }
 
-jz_str* jz_str_substr(jz_str* this, int start, int end) {
+jz_str* jz_str_substr(const jz_str* this, int start, int end) {
   if (end <= start) return jz_str_null();
   return jz_str_new(end - start, this->value + start);
 }
@@ -45,7 +49,7 @@ bool jz_str_equal(const jz_str* s1, const jz_str* s2) {
   return u_strncmp(s1->value, s2->value, s1->length) == 0;
 }
 
-char* jz_str_to_chars(jz_str* this) {
+char* jz_str_to_chars(const jz_str* this) {
   char* to_ret;
   UErrorCode error = U_ZERO_ERROR;
 
--- src/string.h
+++ src/string.h
@@ -5,18 +5,55 @@
 
 #include <stdbool.h>
 
+/* The Jazz string struct and associated functions.
+   jz_strs aren't null-terminated;
+   rather, they're paired with their length.
+   jz_strs are typically manipulated as pointers. */
+
 typedef struct {
   int length;
-  UChar* value;
+  const UChar* value;
 } jz_str;
 
-jz_str* jz_str_new(int length, UChar* value);
-jz_str* jz_str_deep_new(int length, UChar* value);
+/* Creates a new jz_str*.
+   This is shallow,
+   so the 'value' member is the same as the 'value' argument. */
+jz_str* jz_str_new(int length, const UChar* value);
+
+/* Creates a new jz_str*.
+   This is deep,
+   so the character data is copied out of the 'value' argument
+   into a newly allocated 'value' member. */
+jz_str* jz_str_deep_new(int length, const UChar* value);
+
+/* Returns a null jz_str*.
+   This means that 'length' is 0 and 'value' is NULL.
+   The string itself isn't null, though,
+   and must be freed when no longer in use. */
 jz_str* jz_str_null();
-jz_str* jz_str_dup(jz_str* this);
-jz_str* jz_str_deep_dup(jz_str* this);
-jz_str* jz_str_substr(jz_str* this, int start, int length);
+
+/* Creates a shallow duplicate of 'this'.
+   This means that both 'this' and the return value
+   point to the same character string. */
+jz_str* jz_str_dup(const jz_str* this);
+
+/* Creates a deep duplicate of 'this'.
+   This means that the data of the 'value' member of 'this'
+   is copied to a newly allocated 'value' member of the return value. */
+jz_str* jz_str_deep_dup(const jz_str* this);
+
+/* Returns a shallow substring of 'this',
+   beginning at the index 'start'
+   and going through the index 'end'.
+   This is shallow, so the new string is pointing to
+   the same character array as 'this'. */
+jz_str* jz_str_substr(const jz_str* this, int start, int end);
+
+/* Returns whether or not s1 and s2 are equivalent strings. */
 bool    jz_str_equal(const jz_str* s1, const jz_str* s2);
-char*   jz_str_to_chars(jz_str* this);
+
+/* Returns a newly allocated character array containing 'this'
+   transcoded into UTF-8. */
+char*   jz_str_to_chars(const jz_str* this);
 
 #endif
