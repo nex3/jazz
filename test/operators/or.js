0aa0deec780829dfa02b33340b95183847c39470
Nathan Weizenbaum <nex342@gmail.com>
2008-01-06 12:51:01 -0800

    Added unit tests for the or operator.

---
+++ test/operators/or.js
@@ -0,0 +1,10 @@
+return ((true || true) == true) &&
+    ((true || false) == true) &&
+    ((false || true) == true) &&
+    ((false || false) == false) &&
+    ((1 || 2) == 1) &&
+    ((false || 1) == 1) &&
+    ((false || false || true) == true) &&
+    ((false || true || false) == true) &&
+    ((true || false || false) == true) &&
+    ((true || true && false) == true);
