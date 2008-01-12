d870e2bca5221ae630af6dffe9c6624f0be0a809
Nathan Weizenbaum <nex342@gmail.com>
2008-01-12 01:57:35 -0800

    Adding tests for assignment and comma ops, and fixing an error they revealed.

--- src/lex.c
+++ src/lex.c
@@ -244,7 +244,7 @@ void jz_lex_init() {
   state.whitespace_re       = create_re("\\A[\\p{Zs}\\t\\x0B\\f]");
   state.line_terminator_re  = create_re("\\A[\\n\\r\\u2028\\u2029]");
   state.punctuation_re      = create_re("\\A(?:[\\{\\}\\(\\)\\[\\]\\.;,~\\?:]|"
-                                        ">>>|={1,3}|\\+\\+|--|&&|\\|\\||"
+                                        ">>>=?|={1,3}|\\+\\+|--|&&|\\|\\||"
                                         "<<=?|>>=?|!=?=?|\\+=?|-=?|\\*=?|%=?|"
                                         ">=?|<=?|&=?|\\|=?|\\^=?|\\/=?)");
   state.hex_literal_re      = create_re("\\A0[xX][0-9a-fA-F]+");
