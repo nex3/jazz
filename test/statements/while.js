28326608808a5b222f3926ad3b6286d0252ed35f
Nathan Weizenbaum <nex342@gmail.com>
2008-01-13 07:26:49 -0800

    Add a tests for while and do-while loops.

---
+++ test/statements/while.js
@@ -0,0 +1,35 @@
+var res = true;
+
+var a = 0;
+do a++; while (a < 10);
+res = (a == 10) && res;
+
+a = 0;
+do a++; while (0);
+res = (a == 1) && res;
+
+var a = 0, i = 0;
+do {
+    a += 3;
+    i += 1;
+} while (i < 10);
+res = (a == 30) && res;
+
+
+var a = 0;
+while (a < 10) a++;
+res = (a == 10) && res;
+
+a = 0;
+while (0) a = 17;
+res = (a == 0) && res;
+
+var a = 0, i = 0;
+while (i < 10) {
+    a += 3;
+    i += 1;
+}
+res = (a == 30) && res;
+
+while (true) return res;
+return false;
