4611938a64b65b48e17a6000ccae9573ef0e22b7
Nathan Weizenbaum <nex342@gmail.com>
2008-01-13 18:10:35 -0800

    Consolidating the and tests.

---
+++ test/operators/and.js
@@ -0,0 +1,6 @@
+return ((true && true) == true) &&
+    ((true && false) == false) &&
+    ((false && true) == false) &&
+    ((false && false) == false) &&
+    ((true && 12) == 12) &&
+    ((12 && 15) == 15);
