636771abcacf5be20738879e0d6a6a8b39395d37
Nathan Weizenbaum <nex342@gmail.com>
2008-02-05 15:55:27 -0800

    Added support for dot-notation member access.

--- test/expressions/member.js
+++ test/expressions/member.js
@@ -3,9 +3,11 @@ var b = {};
 var c = {};
 a["bar"] = 12;
 b["a"] = a;
-c["i"] = 1;
+c.i = 1;
 c["i"] *= 5;
 return (a["foo"] == undefined) &&
+    (a.foo == undefined) &&
+    (a.bar == 12) &&
     (a["bar"] == 12) &&
     (a["ba" + "r"] == 12) &&
     (b["a"] == a) &&
