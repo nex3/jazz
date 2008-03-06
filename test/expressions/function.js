2fd0dafeab316049467fd97c7e2f72240a337b56
Nathan Weizenbaum <nex342@gmail.com>
2008-03-06 15:18:13 -0800

    Add a failing test for closure stuff.

--- test/expressions/function.js
+++ test/expressions/function.js
@@ -1,6 +1,11 @@
 var a = "hello";
 this.b = "hello";
 var c = function() {};
+var state = (function() { var b; return function(a) {
+    return a == 0 ? function() { return b; } : function (a) { b = a; };
+};})();
+var get = state(0);
+var set = state(1);
 return ((function() { return 12; })() == 12 &&
         (function() { var a = 17; return a + 1; })() == 18 &&
         a == "hello" &&
@@ -12,4 +17,8 @@ return ((function() { return 12; })() == 12 &&
         function() { b = "Woop!"; } &&
         b == "goodbye" &&
         ((function() { a = "blather"; })(), true) &&
-        a == "blather");
+        a == "blather" && 
+        get() == undefined &&
+        (set(12), get()) == 12 &&
+        get() == 12 &&
+        (set("Hello"), get()) == "Hello");
