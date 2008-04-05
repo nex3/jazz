40c853a4be941cdf8089447738fe56d668fbe0ae
Nathan Weizenbaum <nex342@gmail.com>
2008-04-04 23:13:05 -0700

    Add a load function to load JS files.

--- src/core/global.c
+++ src/core/global.c
@@ -25,6 +25,23 @@ jz_val jz_load(JZ_STATE, UFILE* file) {
   return result;
 }
 
+static jz_val load(JZ_STATE, jz_args* args, jz_val arg) {
+  char* filename = jz_str_to_chars(jz, jz_to_str(jz, arg));
+  UFILE* file = u_fopen(filename, "r", NULL, NULL);
+  jz_val result;
+
+  if (file == NULL) {
+    fprintf(stderr, "File does not exist: %s\n", filename);
+    exit(1);
+  }
+
+  free(filename);
+  result = jz_load(jz, file);
+  u_fclose(file);
+
+  return result;
+}
+
 static jz_val is_nan(JZ_STATE, jz_args* args, jz_val arg) {
   double num = jz_to_num(jz, arg);
   return jz_wrap_bool(jz, JZ_NUM_IS_NAN(num));
@@ -34,4 +51,5 @@ void jz_init_global(JZ_STATE) {
   jz_obj* obj = jz->global_obj;
 
   jz_def(jz, obj, "isNaN", is_nan, 1);
+  jz_def(jz, obj, "load", load, 1);
 }
--- test/core/global.js
+++ test/core/global.js
@@ -1,2 +1,3 @@
-return isNaN(NaN) && isNaN("foobar") &&
+load("test/core/_to_load.js");
+return foo == "bar" && isNaN(NaN) && isNaN("foobar") &&
   !isNaN("12") && !isNaN(false) && isNaN();
