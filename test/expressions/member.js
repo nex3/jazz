e66f76d36c0342d9360b4e68cb1db4efc8546127
Nathan Weizenbaum <nex342@gmail.com>
2008-04-07 01:51:18 -0700

    Make test/expressions/member.js JS-test-style.

--- test/expressions/member.js
+++ test/expressions/member.js
@@ -1,18 +1,59 @@
-var a = {};
-var b = {};
-var c = {};
-a["bar"] = 12;
-b["a"] = a;
-c.i = 1;
-c["i"] *= 5;
-return (a["foo"] == undefined) &&
-  (a.foo == undefined) &&
-  (a.bar == 12) &&
-  (a["bar"] == 12) &&
-  (a["ba" + "r"] == 12) &&
-  (b["a"] == a) &&
-  (b["a"]["bar"] == 12) &&
-  ((a["baz"] = 12) == 12) &&
-  (a["baz"] == 12) &&
-  (c["i"] == 5) &&
-  ((c["i"] += 2) == 7);
+load("test/test.js");
+
+test("An undefined property should return `undefined'", function() {
+  assertEqual(undefined, {}.foo);
+});
+
+test("A defined property should return its value", function() {
+  var obj = {};
+
+  obj.foo = 12;
+  assertEqual(12, obj.foo);
+});
+
+test("Bracket-notation and dot-notation property access should both work", function() {
+  var obj = {};
+
+  assertEqual(undefined, obj.foo);
+  assertEqual(undefined, obj["foo"]);
+
+  obj.foo = "bar";
+  assertEqual("bar", obj.foo);
+  assertEqual("bar", obj["foo"]);
+
+  obj["bar"] = "foo";
+  assertEqual("foo", obj.bar);
+  assertEqual("foo", obj["bar"]);
+});
+
+test("Expressions should be nestable within member-expression brackets", function() {
+  var obj = {};
+
+  obj["ba" + "r"] = 12;
+  assertEqual(12, obj.bar);
+});
+
+test("Member expressions should be chainable", function() {
+  var obj = {};
+  obj.foo = {};
+  obj.foo.bar = {};
+  obj.foo.bar.baz = 12;
+
+  assertEqual(12, obj.foo["bar"].baz);
+});
+
+test("Member expressions should work with operator-equals", function() {
+  var obj = {};
+  obj.foo = 1;
+  obj.foo += 2;
+  assertEqual(3, obj.foo);
+});
+
+test("Member assignment expressions should return their values", function() {
+  var obj = {};
+
+  assertEqual(12, obj.foo = 12);
+  assertEqual(13, obj["bar"] = 13);
+});
+
+runTests();
