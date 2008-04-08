3b931287b085b1f8f1d7759288855c5abea17b8a
Nathan Weizenbaum <nex342@gmail.com>
2008-04-08 13:15:22 -0700

    Make test/objects/global.js JS-test-style.

--- test/objects/global.js
+++ test/objects/global.js
@@ -1,7 +1,26 @@
-a = 12;
-this["b"] = 17;
-this["c"] = 19;
-c += 20;
-return (this["a"] == 12) &&
-  (b == 17) &&
-  (c == 39);
+load("test/test.js");
+
+test("Variable declarations without `var' should work", function() {
+  a = 12;
+  assertEqual(12, a);
+});
+
+test("Variable declarations without `var' should declare global variables", function() {
+  (function() { b = "foo"; })();
+  assertEqual("foo", b);
+});
+
+test("The `this' object should provide access to global properties", function() {
+  c = 12;
+  assertEqual(12, this.c);
+  assertEqual(12, this["c"]);
+
+  this.d = "foo";
+  assertEqual("foo", d);
+});
+
+test("The global object should equal itself", function() {
+  assertEqual(this, this);
+});
+
+runTests();
