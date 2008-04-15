a94598bb43a6fa0e34390476350aaf7a3e52a9c9
Nathan Weizenbaum <nex342@gmail.com>
2008-04-15 15:06:30 -0700

    Make test/operators/decrement.js JS-test-style.

--- test/operators/decrement.js
+++ test/operators/decrement.js
@@ -1,5 +1,35 @@
-var a = 1;
-return (a-- == 1) &&
-  (a == 0) &&
-  (--a == -1) &&
-  (a == -1);
+load("test/test.js");
+
+test("Decrementing a variable should decrease its value by 1.", function() {
+  var a = 1;
+  a--;
+  assertEqual(0, a);
+
+  --a;
+  assertEqual(-1, a);
+});
+
+test("Post-decrementing should return the original value.", function() {
+  var a = 1;
+  assertEqual(1, a--);
+});
+
+test("Pre-decrementing should return the new value.", function() {
+  var a = 1;
+  assertEqual(0, --a);
+});
+
+test("Decrementing should perform type conversion.", function() {
+  var a = true;
+  assertEqual(1, a--);
+  assertEqual(0, a);
+});
+
+/*test("Decrementing should work on properties.", function() {
+  var a = {};
+  a.b = 1;
+  a.b--;
+  assertEqual(0, a.b);
+});*/
+
+runTests();
