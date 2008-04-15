9461e398b1ff0fae2e7908e42786dbf38bddcc88
Nathan Weizenbaum <nex342@gmail.com>
2008-04-14 23:40:32 -0700

    Make test/objects/prototype.js JS-test-style.

--- test/objects/prototype.js
+++ test/objects/prototype.js
@@ -1,10 +1,22 @@
+load("test/test.js");
+
 // TODO: Change non-standard ({}).prototype tests to Object.prototype.
-({}).prototype.foo = 12;
-var a = {};
-a.foo = 15;
-var b = {};
-return (({}).foo == 12 &&
-  a.foo == 15 &&
-  a.prototype.foo == 12 &&
-  b.foo == 12 &&
-  b.prototype == ({}).prototype);
+
+test("Setting a property on Object.prototype should make that property available to all objects.", function() {
+  ({}).prototype.foo = 12;
+  assertEqual(12, ({}).foo);
+  assertEqual(12, isNaN.foo);
+});
+
+test("A prototype property should be overridden by local properties.", function() {
+  ({}).prototype.bar = 12;
+  var a = {};
+  a.bar = 15;
+  assertEqual(15, a.bar);
+});
+
+test("Two separate plain objects should have the same prototype.", function() {
+  assertEqual(({}).prototype, ({}).prototype);
+});
+
+runTests();
