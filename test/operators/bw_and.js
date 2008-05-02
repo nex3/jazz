8683bedfee559983cd4312280324a09064be3f55
Nathan Weizenbaum <nex342@gmail.com>
2008-05-02 15:46:47 -0700

    Make test/operators/bw_and.js JS-test-style.

--- test/operators/bw_and.js
+++ test/operators/bw_and.js
@@ -1,10 +1,44 @@
-return ((1 & 2) == 0) &&
-  ((7 & 3) == 3) &&
-  ((-5 & 7) == 3) &&
-  ((-3 & -2) == -4) &&
-  ((2147483650 & 2) == 2) &&
-  ((2147483650 & -2) == -2147483646) &&
-  ((34359738385 & 2) == 0) &&
-  ((34359738385 & -2) == 16) &&
-  ((7 & 3 | -5 & 12) == 11) &&
-  ((7 & 3 ^ -5 & 12) == 11);
+load("test/test.js");
+
+test("Basic bitwise and should work.", function() {
+  assertEqual(0, 1 & 2);
+  assertEqual(3, 7 & 3);
+});
+
+test("A negative integer & a positive integer should be positive.", function() {
+  assertEqual(3, -5 & 7);
+});
+
+test("A negative integer & a negative integer should be negative.", function() {
+  assertEqual(-4, -3 & -2);
+});
+
+test("A 32-bit number & a 2-bit number should give a 2-bit number.", function() {
+  assertEqual(2, 2147483650 & 2);
+});
+
+test("A 32-bit number & a negative 2-bit number should give a negative 32-bit number.", function() {
+  assertEqual(-2147483646, 2147483650 & -2);
+});
+
+test("A 36-bit number & a 2-bit number should give a 2-bit number.", function() {
+  assertEqual(0, 34359738385 & 2);
+});
+
+test("A 36-bit number & a negative 2-bit number should treat the negative as a higher-order bit.", function() {
+  assertEqual(16, 34359738385 & -2);
+});
+
+test("A 36-bit number & another 36-bit number should truncate the two numbers before &-ing.", function() {
+  assertEqual(17, 34359738385 & 34359738385);
+});
+
+test("& should have higher precedence than |.", function() {
+  assertEqual((7 & 3) | (-5 & 12), 7 & 3 | -5 & 12);
+});
+
+test("& should have higher precedence than ^.", function() {
+  assertEqual((7 & 3) ^ (-5 & 12), 7 & 3 ^ -5 & 12);
+});
+
+runTests();
